edcdb8f6cab3ec0586152c636aac62029d97f19d
Stephen More <stephen.more@gmail.com>
2024-09-09 13:51:50 -0400

    Adding these events.

---
+++ mvn/src/main/java/com/pi4j/extensions/events/PositionEvent.java
@@ -0,0 +1,10 @@
+package com.pi4j.extensions.events;
+
+public class PositionEvent {
+
+    public final int position;
+
+    public PositionEvent(int i) {
+        this.position = i;
+    }
+}
---
+++ mvn/src/main/java/com/pi4j/extensions/events/PressEvent.java
@@ -0,0 +1,10 @@
+package com.pi4j.extensions.events;
+
+public class PressEvent {
+
+    public final boolean pressed;
+
+    public PressEvent(boolean pressed) {
+        this.pressed = pressed;
+    }
+}
