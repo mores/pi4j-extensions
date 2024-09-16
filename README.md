0f0b864869a691805550e4bbc6f4dc9810f32a49
Stephen More <stephen.more@gmail.com>
2024-09-16 13:38:55 -0400

    Adds a test for display.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ mvn -DskipTests=true clean install
 mvn -DskipTests=false -Dtest=com.pi4j.extensions.components.SimpleButtonTest test
 
 mvn -DskipTests=false -Dtest=com.pi4j.extensions.devices.i2c.Adafruit5880Test test
+
+sudo mvn -DskipTests=false -Dtest=com.pi4j.extensions.devices.spi.Adafruit3787Test test
---
+++ mvn/src/test/java/com/pi4j/extensions/devices/spi/Adafruit3787Test.java
@@ -0,0 +1,82 @@
+package com.pi4j.extensions.devices.spi;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.exception.Pi4JException;
+import com.pi4j.io.spi.Spi;
+import com.pi4j.io.spi.SpiBus;
+import com.pi4j.io.spi.SpiChipSelect;
+import com.pi4j.io.spi.SpiMode;
+import com.pi4j.io.spi.SpiProvider;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputProvider;
+
+import com.pi4j.extensions.Utils;
+import com.pi4j.extensions.components.LedColor;
+
+public class Adafruit3787Test {
+
+    private static Logger log = LoggerFactory.getLogger(Adafruit3787Test.class);
+
+    private static Context pi4j;
+    private Spi spi;
+
+    DigitalOutput dc;
+    DigitalOutput rst;
+
+    public void setUp() {
+        log.info("setUp");
+
+        pi4j = Pi4J.newAutoContext();
+    }
+
+    public void tearDown() {
+        log.info("tearDown");
+
+        pi4j.shutdown();
+    }
+
+    public void testOne() throws Exception {
+        log.info("testOne");
+
+        final DigitalOutputProvider digitalOutputProvider = pi4j.provider("pigpio-digital-output");
+
+        var spi_config = Spi.newConfigBuilder(pi4j).id("Adafruit3787").name("Display").bus(SpiBus.BUS_0)
+                .chipSelect(SpiChipSelect.CS_0).baud(24000000).mode(SpiMode.MODE_0).build();
+
+        SpiProvider spiProvider = pi4j.provider("pigpio-spi");
+
+        try (var spi = spiProvider.create(spi_config)) {
+
+            // used to indicate which is being sent: data vs command
+            var dc_config = DigitalOutput.newConfigBuilder(pi4j).address(25).build();
+            dc = digitalOutputProvider.create(dc_config);
+
+            Adafruit3787 display = new Adafruit3787(spi, dc);
+
+            display.fill(LedColor.RED);
+            Utils.delay(Duration.ofMillis(1000));
+            display.fill(LedColor.BLUE);
+            Utils.delay(Duration.ofMillis(1000));
+            display.fill(LedColor.GREEN);
+            Utils.delay(Duration.ofMillis(1000));
+            display.fill(LedColor.WHITE);
+            Utils.delay(Duration.ofMillis(1000));
+
+            for (int x = 0; x < 240; x++) {
+                for (int y = 0; y < 240; y++) {
+                    display.pixel(x, y, LedColor.BLACK);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
